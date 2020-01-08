9172fd93d724395992c985b48c7be15823b85282
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-08 19:13:04 +0100

    add generate invoice button

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Button({ btnName = 'Button' }) {
+  return (
+    <span>
+      <GenInvoice style={{backgroundColor: '#27AE60'}}>{btnName}</GenInvoice>
+    </span>
+  )
+}
+
+const GenInvoice = styled.button`
+  font-family: 'Roboto';
+  font-weight: 500;
+  background-color: '#27AE60';
+  color: #fff;
+  border-radius: 1.875rem;
+  text-transform: uppercase;
+  border: none;
+  width: fit-content;
+  margin-top: 2.188rem;
+  margin-bottom: 2.188rem;
+  padding: 0.5625rem 1.563rem;
+`;
+
+export default Button;
