2cca05407b3fe27e0dc16764162792764f3a3a00
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-08 12:44:07 +0100

    create dummy file for links

---
+++ src/data/file.js
@@ -0,0 +1,21 @@
+const file = {
+  navItems: {
+    logoTitle: 'Transmonitor',
+    navLinks: [
+      {
+        name: 'Support',
+        path: '/support'
+      },
+      {
+        name: 'FAQ',
+        path: '/faq'
+      },
+      {
+        name: 'Notification',
+        path: '/notification'
+      }
+    ]
+  },
+}
+
+export default file;
