53280586d955093166f89727a6be1849dc5d4bfb
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-08 12:45:08 +0100

    style logo as link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+import Heading1 from './Heading1';
 
-function Logo() {
+function Logo({ title }) {
   return (
-    <span>
-      <h1>Transmonitor</h1>
-    </span>
+    <LogoSpan>
+      <Heading1 title={title} />
+    </LogoSpan>
   )
 }
+
+const LogoSpan = styled.span`
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default Logo;
