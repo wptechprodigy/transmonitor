3017b3f1ccf27bc707205fc9bbfe71b3e1fcb24e
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-08 16:40:36 +0100

    create greeting component

---
+++ src/components/Greeting.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+function Greeting({ greeting }) {
+  return <P>{greeting}</P>
+}
+
+Greeting.propTypes = {
+  greeting: PropTypes.string.isRequired
+}
+
+const P = styled.p`
+  font-family: 'Segoe UI';
+  display: 'block';
+  color: #647787;
+  font-size: 0.625rem;
+  line-height: 0.8125rem;
+`;
+
+export default Greeting;
