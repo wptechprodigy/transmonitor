243cd74f1aa7e776281c6ae4a7936af8c3a0e4e2
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-08 20:11:31 +0100

    create menu category component
    This displays the category of names on the sidebar

---
+++ src/components/MenuCategory.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+function MenuCategory({ menuName = 'Menu' }) {
+  return <H3>{menuName}</H3>
+}
+
+MenuCategory.propTypes = {
+  menuName: PropTypes.string.isRequired
+}
+
+const H3 = styled.h3`
+  font-family: 'Segoe UI';
+  font-size: 0.6875rem;
+  line-height: 0.9375rem;
+  color: #647787;
+`;
+
+export default MenuCategory;
