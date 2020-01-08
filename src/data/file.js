3ba4d6c683fb879c9d9ff3d791e06e46876b3f3c
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-08 20:14:33 +0100

    update dummy data in file

--- src/data/file.js
+++ src/data/file.js
@@ -1,3 +1,9 @@
+import overview from '../assets/overview.svg';
+import walletAllpayment from '../assets/wallet-allpayment.svg';
+import walletReconpayment from '../assets/wallet-reconpayment.svg';
+import walletUnreconpayment from '../assets/wallet-unreconpayment.svg';
+import manualSettlements from '../assets/manual-settlements.svg';
+
 const file = {
   navItems: {
     logoTitle: 'Transmonitor',
@@ -12,6 +18,69 @@ const file = {
       }
     ]
   },
+  sideBarItems: {
+    mainCategory: {
+      name: 'Main',
+      links: [
+        {
+          icon: overview,
+          linkName: 'Overview',
+          path: '/',
+          active: 'true'
+        }
+      ]
+    },
+    paymentCategory: {
+      name: 'Payment',
+      links: [
+        {
+          icon: walletAllpayment,
+          linkName: 'All Payments',
+          path: '/'
+        },
+        {
+          icon: walletReconpayment,
+          linkName: 'Reconcilled Payments',
+          path: '/reconcilled'
+        },
+        {
+          icon: walletUnreconpayment,
+          linkName: 'Un-Reconcilled Payments',
+          path: '/unreconcilled'
+        },
+        {
+          icon: manualSettlements,
+          linkName: 'Manual Settlements',
+          path: '/manual-settlements'
+        },
+      ]
+    },
+    OrdersCategory: {
+      name: 'Orders',
+      links: [
+        {
+          icon: '',
+          linkName: 'All Orders',
+          path: '/'
+        },
+        {
+          icon: '',
+          linkName: 'Pending Orders',
+          path: '/pending-orders'
+        },
+        {
+          icon: '',
+          linkName: 'Reconcilled Orders',
+          path: '/reconcilled-orders'
+        },
+        {
+          icon: '',
+          linkName: 'Merchant Profile',
+          path: '/merchant-profile'
+        },
+      ]
+    }
+  }
 }
 
 export default file;
