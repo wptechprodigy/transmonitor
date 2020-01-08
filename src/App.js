5618b072a2bba60f3f953326d530d812f953836d
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-08 12:45:50 +0100

    modify file for proper setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import styled from 'styled-components';
+import NavBar from './components/NavBar';
+
+import file from './data/file'
 
 function App() {
+  const { navItems } = file;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar navItems={navItems} />
+    </Router>
   );
 }
+
+// const Section = styled.section`
+//   display: grid;
+//   grid-template-rows: 60px auto;
+// `;
 
 export default App;
