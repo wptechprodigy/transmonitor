22262c91c59c97494a5c4351b473500cbe83d5c9
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-08 16:41:14 +0100

    update nav menu with bell as notification

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import bell from '../assets/bell.svg';
+
 import NavLink from './NavLink';
+import Icon from './Icon';
 
 function NavMenu({ navLinks }) {
   return (
     <NavListSection>
       {navLinks.map(({ name, path }) => <NavLink key={name} name={name} path={path} />)}
+      <Icon name={bell} />
     </NavListSection>
   )
 }
