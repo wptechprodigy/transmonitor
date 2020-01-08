9032799fb5fc5665dcc7f34a3d2a632f95585557
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-08 20:30:43 +0100

    modify dummy data
    Added imports for Orders category of the side bar menus

--- src/data/file.js
+++ src/data/file.js
@@ -3,6 +3,11 @@ import walletAllpayment from '../assets/wallet-allpayment.svg';
 import walletReconpayment from '../assets/wallet-reconpayment.svg';
 import walletUnreconpayment from '../assets/wallet-unreconpayment.svg';
 import manualSettlements from '../assets/manual-settlements.svg';
+import allOrders from '../assets/all-orders.svg';
+import pendingOrders from '../assets/pending-orders.svg';
+import reconcilledOrders from '../assets/reconcilled-orders.svg';
+import merchantProfile from '../assets/merchant-profile.svg';
+
 
 const file = {
   navItems: {
@@ -36,17 +41,17 @@ const file = {
         {
           icon: walletAllpayment,
           linkName: 'All Payments',
-          path: '/'
+          path: '/all-payments'
         },
         {
           icon: walletReconpayment,
           linkName: 'Reconcilled Payments',
-          path: '/reconcilled'
+          path: '/reconcilled-payments'
         },
         {
           icon: walletUnreconpayment,
           linkName: 'Un-Reconcilled Payments',
-          path: '/unreconcilled'
+          path: '/unreconcilled-payments'
         },
         {
           icon: manualSettlements,
@@ -59,22 +64,22 @@ const file = {
       name: 'Orders',
       links: [
         {
-          icon: '',
+          icon: allOrders,
           linkName: 'All Orders',
-          path: '/'
+          path: '/all-orders'
         },
         {
-          icon: '',
+          icon: pendingOrders,
           linkName: 'Pending Orders',
           path: '/pending-orders'
         },
         {
-          icon: '',
+          icon: reconcilledOrders,
           linkName: 'Reconcilled Orders',
           path: '/reconcilled-orders'
         },
         {
-          icon: '',
+          icon: merchantProfile,
           linkName: 'Merchant Profile',
           path: '/merchant-profile'
         },
