931216b7ae2befd918a999d4e7f2cf620d182f4c
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-09 01:15:27 +0100

    create a reusable transaction summary
    Created a component to display transaction summaries.
    
    It's made reusable so as to be used for other summaries.

---
+++ src/components/EachTransactionSummary.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import SummaryLabel from './SummaryLabel';
+import SummaryValue from './SummaryValue';
+
+function EachTransactionSummary({ summaryDetail }) {
+  return (
+    <Section>
+      {summaryDetail.map(({ label, value }) => {
+        return (
+          <Span key={label}>
+            <div>
+              <SummaryLabel label={label} />
+              <SummaryValue value={value} />
+            </div>
+            <div>
+              <img src='chart' alt='chart' />
+            </div>
+          </Span>
+        )
+      })}
+    </Section>
+  )
+}
+
+const Section = styled.section`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Span = styled.span`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 280px;
+  height: 75px;
+  width: 100%;
+  background: #FFFFFF;
+  border: 1px solid #EEF8FD;
+  box-sizing: border-box;
+  border-radius: 2px;
+  margin-left: 35px;
+  padding: 18px;
+
+  & div {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export default EachTransactionSummary;
