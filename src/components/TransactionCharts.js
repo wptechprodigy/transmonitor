09b3bac5a729c38a751aff3cb925989bfd2a7db2
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-10 21:28:57 +0100

    mofidy transaction chart

--- src/components/TransactionCharts.js
+++ src/components/TransactionCharts.js
@@ -10,21 +10,21 @@ function TransactionCharts({ transactions }) {
 
   return (
     <Section>
-      <span>
+      <Chart>
         <p>Chart goes here</p>
-      </span>
-      <span>
-        <span>
+      </Chart>
+      <TransactionOverview>
+        <OrderOverview>
           <Heading3 title='Orders' />
           <ProgressBar percent={Number(orders[1].value)} />
           {orders.map(({ label, value, color }) => <TransactionLabel key={label} color={color} label={label} value={value} />)}
-        </span>
-        <span>
+        </OrderOverview>
+        <OrderOverview>
           <Heading3 title='Payments' />
           <ProgressBar percent={Number(payments[1].value)} />
           {payments.map(({ label, value, color }) => <TransactionLabel key={label} color={color} label={label} value={value} />)}
-        </span>
-      </span>
+        </OrderOverview>
+      </TransactionOverview>
     </Section>
   );
 }
@@ -36,13 +36,31 @@ const Section = styled.section`
   min-height: 330px;
   background: #FFFFFF;
   border: 1px solid #EEF8FD;
-  box-sizing: border-box;
   border-radius: 2px;
   margin-top: 30px;
+`;
 
-  & span:first-child {
-    width: 65%;
+const Chart = styled.section`
+  width: 65%;
+  border-right: 4px solid #EEF8FD;
+  padding-left: 30px;
+  padding-right: 30px;
+`;
+
+const TransactionOverview = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 35%;
+
+  & section:first-child {
+    border-bottom: 4px solid #EEF8FD;
   }
+`;
+
+const OrderOverview = styled.section`
+
+  height: 100%;
+  padding-left: 20px;
 `;
 
 export default TransactionCharts;
