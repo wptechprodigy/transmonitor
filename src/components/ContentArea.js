92242a0d42975487cc31756902e66fad18556408
wptechprodigy <waheed.freelancer16@gmail.com>
2020-01-10 20:24:21 +0100

    refactor code

--- src/components/ContentArea.js
+++ src/components/ContentArea.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+
 import Button from './Button';
 import SideBarLinks from './SideBarLinks';
 import TransactionsSummary from './TransactionsSummary';
